656543e45bf315636da23548ce39a9b5f48c0713
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-27 15:20:49 -0300

    feat: solution of the exercise 7 (list 2)

---
+++ papi/lista2/exercicio7.py
@@ -0,0 +1,18 @@
+import math
+
+print("Aqui a área de um quadrado e o dobro desta área.")
+
+while True:
+    try:
+        lado = float(input("Por favor favor informe o lado do quadrado em centímetros - use o ponto como separador decimal: "))
+        
+        if lado < 0:
+            raise ValueError
+
+        break
+    except ValueError:
+        print("Por favor informe um número válido e maior que zero.")
+
+area = lado * lado
+print("A área do quadrado em centímetros é", area)
+print("O dobro da área do quadrado em centímetros é", area * 2)
