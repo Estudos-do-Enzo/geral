4d53b7571639c01cf8babbbdcd84d023c4efb431
André de Paula Terceiro <andre@terceiro.com.br>
2025-01-02 14:32:46 -0300

    feat: solution of the exercise 5 - list 1 - in Kotlin

---
+++ papi/lista1/exercicio5.kt
@@ -0,0 +1,38 @@
+package lista1
+
+fun main() {
+    var salarioMinimo: Double = 0.0
+    var salarioUsuario: Double = 0.0
+
+    while (true) {
+        try {
+            print("Por favor digite o salário mínimo vigente, usando o ponto como separador decimal: ")
+            var input = readLine()
+            salarioMinimo = input!!.toDouble()
+            if (salarioMinimo <= 0) {
+                throw Exception("Número inválido")
+            }
+            break
+        } catch (e: Exception) {
+            println("Por favor digite um valor maior ou igual a zero")
+        }
+    }
+
+    while (true) {
+        try {
+            print("Por favor digite o seu salário, usando o ponto como separador decimal: ")
+            var input = readLine()
+            salarioUsuario = input!!.toDouble()
+            if (salarioMinimo <= 0) {
+                throw Exception("Número inválido")
+            }
+            break
+        } catch (e: Exception) {
+            println("Por favor digite um valor maior ou igual a zero")
+        }
+    }
+
+    print("Você ganha ")
+    print(salarioUsuario / salarioMinimo)
+    println(" salários mínimos")
+}
