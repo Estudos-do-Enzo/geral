b8a9d43a7ca1b1d3a1fa28a9694c3884fa537d0e
André de Paula Terceiro <andre@terceiro.com.br>
2025-01-02 21:20:43 -0300

    feat: adjusting the solution of the exercise 9 (list 1) in Kotlin

--- papi/lista1/exercicio9.kt
+++ papi/lista1/exercicio9.kt
@@ -7,7 +7,7 @@ fun main() {
 
     while (true) {
         try {
-            print("Digite a altura: ")
+            print("Digite a altura em metros: ")
             altura = readLine()!!.toDouble()
             if (altura <= 0) {
                 throw NumberFormatException("Altura inválida. Por favor, digite um valor maior que zero.")
@@ -20,7 +20,7 @@ fun main() {
 
     while(true) {
         try {
-            print("Digite o peso: ")
+            print("Digite o peso em kilos: ")
             peso = readLine()!!.toDouble()
             if (peso <= 0) {
                 throw NumberFormatException("Peso inválido. Por favor, digite um valor maior que zero")
@@ -33,4 +33,19 @@ fun main() {
 
     val imc = peso / (altura * altura)
     println("Para os valores passados, o IMC é: $imc")
+    print("O resultado da textual de uma avaliação é: ")
+
+    if (imc < 18.5) {
+        println("abaixo do peso")          
+    } else if (imc < 25) {
+        println("peso ideal (parabéns)")
+    } else if (imc < 30) {
+        println("levemente acima do peso")
+    } else if (imc < 35) {
+        println("obesidade grau I")
+    } else if (imc < 40) {
+        println("obesidade grau II (severa)")
+    } else {
+        println("obesidade grau III (mórbida)")
+    }
 }
